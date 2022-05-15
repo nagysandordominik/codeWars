95a27274f51cc90ca55f59cf3885b5da128c060e
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-05-15 21:34:04 +0200

    solution admit

---
+++ remove-string-spaces.js
@@ -0,0 +1,8 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+    const resultant = x.replace(/ /g,'')
+    return resultant;
+}
+
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')
