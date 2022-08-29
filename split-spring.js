471a8de4840c00582558d28d6041b4246271368f
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-08-29 15:03:32 +0200

    solution

---
+++ split-spring.js
@@ -0,0 +1,12 @@
+function solution(str){
+    let i = 0;
+    let result = new Array();
+    if (str.length % 2 !== 0) {
+      str = str + '_';
+    }
+    while (i < str.length) {
+        result.push(str[i] + str[i+1]);
+        i += 2;
+      }
+    return result;
+  }
